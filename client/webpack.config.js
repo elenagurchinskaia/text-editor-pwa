1258cf1903dd30bac0a6b04dc61824e7ba0053cb
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-25 17:22:55 -0500

    module rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,19 +48,19 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
