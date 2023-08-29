e588c70f0450e6fa30daaa5f6980fdcc0479025a
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-28 22:17:34 -0500

    added comments to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+// import openDB function from the idb module
 import { openDB } from "idb";
 
 const initdb = async () =>
@@ -12,25 +13,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// add content to the database
 export const putDb = async (content) => {
+  // open the jate database with version 1
   const db = await openDB("jate", 1);
+  // starts a readwrite transaction on the jate obj store
   const tx = db.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
+  // use put method of the obj. store to add an entry with an object containing properties id and content
   const request = store.put({ id: 1, content: content });
-  // const result = await request;
-  // await tx.complete;
   console.log("Notes saved successfully!");
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// get all the content from the database
 export const getDb = async () => {
+  // open the jate database with version 1
   const db = await openDB("jate", 1);
+  // start the readonly transaction on the jate object store
   const tx = db.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
+  // retreive an entry with the specified id
   const request = await store.get(1);
+  // return the content property of the retreived entry
   return request.content;
 };
 
 initdb();
-// activity 26
